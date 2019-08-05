4947234c3c5a9cb1665f95ee62075f833f799433
yfqlzlx <zwt135689>
2019-08-05 19:22:38 +0800

    初始化

---
+++ src/main/java/org/yf/qy/QyApplication.java
@@ -0,0 +1,13 @@
+package org.yf.qy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/yf/qy/config/SpringmvcConfig.java
@@ -0,0 +1,22 @@
+package org.yf.qy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author yfqlzlx
+ * @date 2019/4/8 16:10
+ *
+ * springmvc的静态资源映射
+ */
+@Configuration
+public class SpringmvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 工程静态文件映射
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        // 本地图片映射
+        registry.addResourceHandler("/api/goods/img/**").addResourceLocations("file:D:/素材/gd/");
+    }
+}
